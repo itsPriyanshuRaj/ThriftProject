2af466e8439b70be904e8d1400ab10005a33a795
itsPriyanshuRaj <priyanshuraj14raj@gmail.com>
2022-09-28 11:45:51 +0530

    routing module

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,23 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { AdminComponent } from './components/admin/admin.component';
 import { CartComponent } from './components/cart/cart.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 import { ProductComponent } from './components/product/product.component';
+import { ShopComponent } from './components/shop/shop.component';
+import { UserpanelComponent } from './components/userpanel/userpanel.component';
 
 const routes: Routes = [
   {
-    path: '', component:HomeComponent
+    path: '', component: HomeComponent
   },
   {
-    path: 'product/:id', component:ProductComponent
+    path: 'about', component: AboutComponent
   },
   {
-    path: 'cart', component:CartComponent
+    path: 'shop', component:ShopComponent
   },
   {
-    path: 'checkout', component:CheckoutComponent
+    path: 'product', component: ProductComponent
+  },
+  {
+    path: 'cart', component: CartComponent
+  },
+  {
+    path: 'checkout', component: CheckoutComponent
+  },
+  {
+    path: 'login', component:LoginComponent
+  },
+  {
+    path: 'admin', component:AdminComponent
+  },
+  {
+    path: 'user', component:UserpanelComponent
   }
+
 ];
 
 @NgModule({
