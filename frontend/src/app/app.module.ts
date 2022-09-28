ee8945dcb86ea07d21ee14aab1c2a0494485c105
itsPriyanshuRaj <priyanshuraj14raj@gmail.com>
2022-09-28 11:49:50 +0530

    module file

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -9,6 +9,12 @@ import { HomeComponent } from './components/home/home.component';
 import { CartComponent } from './components/cart/cart.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { ProductComponent } from './components/product/product.component';
+import { AboutComponent } from './components/about/about.component';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { ShopComponent } from './components/shop/shop.component';
+import { UserpanelComponent } from './components/userpanel/userpanel.component';
 
 @NgModule({
   declarations: [
@@ -18,11 +24,27 @@ import { ProductComponent } from './components/product/product.component';
     HomeComponent,
     CartComponent,
     CheckoutComponent,
-    ProductComponent
+    ProductComponent,
+    AboutComponent,
+    LoginComponent,
+    AdminComponent,
+    ShopComponent,
+    UserpanelComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: '', 
+        component:HomeComponent
+      },
+      {
+        path: './about.component.html',
+        component:AboutComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
