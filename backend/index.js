a863bccc9c05c3f2687c75885210446b8d487dcc
PriyanshuRaj <73906501+itsPriyanshuRaj@users.noreply.github.com>
2022-10-06 23:09:26 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,46 +1,60 @@
+const bodyParser = require('body-parser');
 var express = require('express');
-var mysql = require('mysql');
 var app = express();
+var port = 3000
+var routerCategory = require('./routes/category');
 
-var connection = mysql.createPool({
-  connectionLimit: 100,
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'thrift'
+
+app.use(function(req,res,next){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 
-app.get('/', function (req, res) {
-  //about mysql
-  connection.getConnection(function (error, tempCont) {
 
-    if (!!error) {
-      tempCont.release();
-      console.log("Error");
-    } else {
-      console.log("Connected with database");
+app.use(bodyParser.json());
+app.use('./routes/category', routerCategory);
 
-
-      tempCont.query(`SELECT * FROM product`, function (error, rows, fields) {
-        tempCont.release();
-        if (!!error) {
-          console.log("Error in the query");
-        } else {
-          res.json(rows);
-        }
-      })
-    }
-  });
-
-
-  // connection.query(`SELECT * FROM product`, function(error,rows,fields){
-  //   if(!!error){
-  //     console.log("Error in the query");
-  //   } else{
-  //     console.log("Successfull query");
-  //     console.log(rows);
-  //   }
-  // });
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 })
-app.listen(8080);
+
+
+
+
+
+
+
+// var mysql = require('mysql');
+// var connection = mysql.createPool({
+//   connectionLimit: 100,
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'thrift'
+// });
+
+// app.get('/', function (req, res) {
+//   //about mysql
+//   connection.getConnection(function (error, tempCont) {
+
+//     if (!!error) {
+//       tempCont.release();
+//       console.log("Error");
+//     } else {
+//       console.log("Connected with database");
+
+
+//       tempCont.query(`SELECT * FROM product`, function (error, rows, fields) {
+//         tempCont.release();
+//         if (!!error) {
+//           console.log("Error in the query");
+//         } else {
+//           res.json(rows);
+//         }
+//       })
+//     }
+//   });
+// })
